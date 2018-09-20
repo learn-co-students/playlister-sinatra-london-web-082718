5e2231f7e039719926d67689f89b725c9c13896c
github username <github email address>
2018-09-20 18:18:29 +0100

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,13 @@ class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+
+  def slug
+    self.name.downcase.split.join("-")
+  end
+
+  def self.find_by_slug(slug)
+    slug = slug.split('-').join(' ')
+    Genre.where('name LIKE ?', slug)[0]
+  end
 end
