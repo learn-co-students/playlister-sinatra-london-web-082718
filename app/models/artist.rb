ae43312d20bf724b5ac348dfcd962ec4133c7b82
thexyman <makdaa1@gmail.com>
2018-09-21 19:43:44 +0100

    Done.

---
+++ app/models/artist.rb
@@ -0,0 +1,14 @@
+class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :song_genres, through: :songs
+  has_many :genres, through: :song_genres
+
+  def slug
+    self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(slug)
+    all.find {|song| song.slug == slug }
+  end
+  
+end
